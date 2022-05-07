57427d23796cb7d857b7d210548ca4cc2b7dfbae
Hua625 <cyh20030625@gmail.com>
2022-05-07 17:32:20 +0800

    factory

--- Chinese_Chess/Chinese_Chess/Chess.h
+++ Chinese_Chess/Chinese_Chess/Chess.h
@@ -22,8 +22,70 @@ public:
 	virtual void move() = 0;
 };
 
+class King :public Chess
+{
+public:
+	virtual void move() override;
+};
+class Advisors :public Chess
+{
+public:
+	virtual void move() override;
+};
+class Minister :public Chess
+{
+public:
+	virtual void move() override;
+
+};
+class Chariots :public Chess
+{
+public:
+	virtual void move() override;
+
+};
+class Knights :public Chess
+{
+public:
+	virtual void move() override;
+
+};
+class Cannons :public Chess
+{
+public:
+	virtual void move() override;
+
+};
+class Soldiers :public Chess
+{
+public:
+	virtual void move() override;
+	
+};
+
+
 class Factory {
 	//工廠返回商品
 public:
-	virtual Chess* getProduct() = 0;
+	Chess* getChess(Characters ch)
+	{
+		switch (ch)
+		{
+		case Characters::King:
+			return new King;
+		case Characters::Advisors:
+			return new Advisors;
+		case Characters::Minister:
+			return new Minister;
+		case Characters::Chariots:
+			return new Chariots;
+		case Characters::Knights:
+			return new Knights;
+		case Characters::Cannons:
+			return new Cannons;
+		case Characters::Soldiers:
+			return new Soldiers;
+		}
+	}
+
 };
