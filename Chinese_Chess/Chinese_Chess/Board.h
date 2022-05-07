70976830f714c603ed99d4face91fa4ca8000bd0
Hua625 <cyh20030625@gmail.com>
2022-05-07 16:24:50 +0800

    test

---
+++ Chinese_Chess/Chinese_Chess/Board.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+
+
+class Board
+{
+private:
+	sf::Texture textTure;
+	sf::Sprite sprite;
+public:
+	Board();
+};
+
