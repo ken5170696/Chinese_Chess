32e9c33375d23216a3cf50cd15632d2b43289e89
Hua625 <cyh20030625@gmail.com>
2022-05-07 15:33:31 +0800

    Game class

--- Chinese_Chess/Chinese_Chess/Source.cpp
+++ Chinese_Chess/Chinese_Chess/Source.cpp
@@ -1,24 +1,9 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
+#include "GameManager.h"
+
 
 int main()
 {
-	sf::RenderWindow w(sf::VideoMode(600, 800), "ok");
-	sf::Event ev;
-	while (w.isOpen())
-	{
-		while (w.pollEvent(ev))
-		{
-			switch (ev.type)
-				case sf::Event::Closed:
-					w.close();
-					break;
-		}
-		w.clear();
-		w.display();
+	Game game;
 
-	}
+	game.run();
 }
