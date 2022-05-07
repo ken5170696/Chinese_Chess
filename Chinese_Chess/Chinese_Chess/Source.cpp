5a31d2d712dbff4fbeacf4ac9b3e6649910e026e
庭宇 劉 <tingyuliu0223@gmail.com>
2022-05-07 15:43:24 +0800

    Add photo

--- Chinese_Chess/Chinese_Chess/Source.cpp
+++ Chinese_Chess/Chinese_Chess/Source.cpp
@@ -6,19 +6,5 @@
 
 int main()
 {
-	sf::RenderWindow w(sf::VideoMode(600, 800), "ok");
-	sf::Event ev;
-	while (w.isOpen())
-	{
-		while (w.pollEvent(ev))
-		{
-			switch (ev.type)
-				case sf::Event::Closed:
-					w.close();
-					break;
-		}
-		w.clear();
-		w.display();
 
-	}
 }
